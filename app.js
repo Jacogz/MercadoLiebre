a46d0f6053fa62df9175aa6ffe09b0d1382bf5ee
Jacog712 <giraldozuluagajm@gmail.com>
2022-08-19 14:23:42 -0500

    finalfinal

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const app = express();
 const path = require('path');
 const publicPath = path.resolve(__dirname, './public')
 app.use(express.static(publicPath))
-app.listen(3030, ()=>console.log('online'));
+app.listen(process.env.PORT || 3000, ()=>console.log('online'));
 app.get('/', (req, res)=>res.sendFile(path.resolve(__dirname, './views/home.html')))
 app.get('/register', (req,res)=>res.sendFile(path.resolve(__dirname, './views/register.html')))
